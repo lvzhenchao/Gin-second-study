53e87638d08e467dcea606326f5a0faa921248b4
lvzhenchao <1370164516@qq.com>
2021-10-08 22:30:49 +0800

    Create 2、post请求.go

---
+++ 2、post请求.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main()  {
+	r := gin.Default()
+	r.POST("/post", postMsg)
+	r.Run(":9090")
+}
+
+func postMsg(c *gin.Context)  {
+	//name := c.Query("name")
+
+	//name := c.PostForm("name")//获取表单数据
+	name := c.DefaultPostForm("name", "lzc")//获取表单数据，并有默认值
+
+	form, b := c.GetPostForm("name")
+	fmt.Println(form, b)
+
+	c.JSON(http.StatusOK, gin.H{
+		"name": name,
+		"code": http.StatusOK,
+	})
+}
