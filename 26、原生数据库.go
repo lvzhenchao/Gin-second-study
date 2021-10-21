a7a350eacb7e00df064cd6c42053086792f19ce5
lvzhenchao <1370164516@qq.com>
2021-10-21 22:48:38 +0800

    Update 26、原生数据库.go

--- 26、原生数据库.go
+++ 26、原生数据库.go
@@ -47,10 +47,96 @@ type SqlResponse struct {
 func main()  {
 	r := gin.Default()
 
-	r.POST("sql/insert", insertData)//写数据
+	r.POST("sql/insert", insertData)//添加数据
 	r.GET("sql/get", getData)//查询单条数据
+	r.GET("sql/mulget", getMulData)//查询多条数据
+	r.PUT("sql/update", updateData)//更新数据
+	r.DELETE("sql/delete", deleteData)//删除数据
 
 	r.Run(":9090")
+}
+
+func deleteData(c *gin.Context) {
+	name := c.Query("name")
+	//先查询是否存在
+	var count int
+	sqlStr := "select count(*) from user where name = ?"
+	err := sqlDb.QueryRow(sqlStr, name).Scan(&count)
+	if count <= 0 || err !=nil {
+		sqlResponse.Code = http.StatusBadRequest
+		sqlResponse.Message = "数据不存在"
+		sqlResponse.Data = "error"
+		c.JSON(http.StatusOK, sqlResponse)
+		return
+	}
+
+	delStr := "delete from user where name=?"
+	ret, err := sqlDb.Exec(delStr, name)
+	if err !=nil {
+		sqlResponse.Code = http.StatusBadRequest
+		sqlResponse.Message = "删除失败"
+		sqlResponse.Data = "error"
+		c.JSON(http.StatusOK, sqlResponse)
+		return
+	}
+	sqlResponse.Code = http.StatusOK
+	sqlResponse.Message = "删除成功"
+	sqlResponse.Data = "OK"
+	c.JSON(http.StatusOK, sqlResponse)
+	fmt.Println(ret.LastInsertId())//打印结果
+}
+
+func updateData(c *gin.Context) {
+	var u SqlUser
+	err := c.Bind(&u)
+	if err != nil {
+		sqlResponse.Code = http.StatusBadRequest
+		sqlResponse.Message = "参数错误"
+		sqlResponse.Data = "error"
+		c.JSON(http.StatusOK, sqlResponse)
+		return
+	}
+	sqlStr := "update user set address=?, age = ? where name = ?"
+	ret, err := sqlDb.Exec(sqlStr, u.Address, u.Age, u.Name)
+	if err != nil {
+		fmt.Printf("更新错误：err:%v\n", err)
+		sqlResponse.Code = http.StatusBadRequest
+		sqlResponse.Message = "更新失败"
+		sqlResponse.Data = "error"
+		c.JSON(http.StatusOK, sqlResponse)
+		return
+	}
+
+	sqlResponse.Code = http.StatusOK
+	sqlResponse.Message = "更新成功"
+	sqlResponse.Data = "OK"
+	c.JSON(http.StatusOK, sqlResponse)
+	fmt.Println(ret.LastInsertId())//打印结果
+}
+
+func getMulData(c *gin.Context) {
+	address := c.Query("address")
+	sqlStr := "select name, age from user where address = ?"
+	rows, err := sqlDb.Query(sqlStr, address)
+	if err != nil {
+		sqlResponse.Code = http.StatusBadRequest
+		sqlResponse.Message = "查询错误"
+		sqlResponse.Data = "error"
+		c.JSON(http.StatusOK, sqlResponse)
+		return
+	}
+	defer rows.Close()
+	resUser := make([]SqlUser, 0)
+	for rows.Next() {
+		var userTemp SqlUser
+		rows.Scan(&userTemp.Name, &userTemp.Age)
+		userTemp.Address = address
+		resUser = append(resUser, userTemp)
+	}
+	sqlResponse.Code = http.StatusOK
+	sqlResponse.Message = "读取成功"
+	sqlResponse.Data = resUser
+	c.JSON(http.StatusOK, sqlResponse)
 }
 
 func getData(c *gin.Context) {
