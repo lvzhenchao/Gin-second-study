82b91975932adc5b9b7d1f7ed2d36144def58276
lvzhenchao <1370164516@qq.com>
2021-10-28 22:47:09 +0800

    中间件

---
+++ Gin_App_Api/middleware/RecoverMiddleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"Gin-second-study/Gin_App_Api/response"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func RecoverMiddleware() gin.HandlerFunc  {
+	return func(c *gin.Context) {
+		defer func() {
+			err := recover()
+			if err != nil {
+				response.Fail(c, nil, fmt.Sprint(err))
+				c.Abort()
+				return
+			}
+		}()
+	}
+}
