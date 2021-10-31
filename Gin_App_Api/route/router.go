8cfb13428b02dc30b572ddef12a7f6b6995c3809
lvzhenchao <1370164516@qq.com>
2021-10-31 21:34:10 +0800

    Update router.go

--- Gin_App_Api/route/router.go
+++ Gin_App_Api/route/router.go
@@ -11,6 +11,9 @@ func CollectRouter(r *gin.Engine)*gin.Engine  {
 	r.POST("/api/auth/register", controller.Register)//注册
 	r.POST("/api/auth/login",controller.Login)//登录
 	r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info)//再传递数据
+
+
+	return r
 }
 
 
